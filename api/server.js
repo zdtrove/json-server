b679ab62d1734a40cef71f3ac2bfd7264ba6ee7d
Hoang An <antnh@icd-vn.com>
2024-12-03 08:55:06 +0700

    Commit update code

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const cors = require('cors');
 
 const server = jsonServer.create()
 
@@ -16,6 +17,7 @@ const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
+server.use(cors());
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
